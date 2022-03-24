cb9c0c4a568bef2f1832e3f417e3220171de0734
Rafael Martinez Bermudez <rafaelmartinez2699@gmail.com>
2022-03-24 07:12:19 -0500

    Create Product page

--- ecommerce_django_vue/djackets_vue/src/router/index.js
+++ ecommerce_django_vue/djackets_vue/src/router/index.js
@@ -1,11 +1,12 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import Product from '../views/Product.vue';
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
   },
   {
     path: '/about',
@@ -13,13 +14,19 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+  },
+  {
+    path: '/:category_slug/:product_slug',
+    name: 'Product',
+    component: Product,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
--- ecommerce_django_vue/product/urls.py
+++ ecommerce_django_vue/product/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 
 from product import views
-from .views import LatestProductsList
 
 urlpatterns = [
-    path('latest-products/', view=LatestProductsList.as_view())
+    path('latest-products/', view=views.LatestProductsList.as_view()), 
+    path('products/<slug:category_slug>/<slug:product_slug>/',view=views.ProductDetail.as_view())
 ]
