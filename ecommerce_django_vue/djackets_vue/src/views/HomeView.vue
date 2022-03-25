1d26660e38e397e69220e5e12de4e5369a7b7466
Rafael Martinez Bermudez <rafaelmartinez2699@gmail.com>
2022-03-25 07:50:14 -0500

    Create category view from API and Vue app

--- ecommerce_django_vue/djackets_vue/src/views/HomeView.vue
+++ ecommerce_django_vue/djackets_vue/src/views/HomeView.vue
@@ -10,31 +10,18 @@
       <div class="column is-12">
         <h2 class="is-size-2 has-text-centered">Latest Products</h2>
       </div>
-      <div
-        class="column is-3"
+      <ProductBox
         v-for="product in latestProducts"
         v-bind:key="product.id"
-      >
-        <div class="box">
-          <figure class="image mb-4">
-            <img v-bind:src="product.get_thumbnail" />
-          </figure>
-          <h3 class="is-size-4">{{ product.name }}</h3>
-          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
-          <router-link
-            v-bind:to="product.get_absolute_url"
-            class="button is-dark mt-4"
-          >
-            View Details
-          </router-link>
-        </div>
-      </div>
+        v-bind:product="product"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import ProductBox from '@/components/ProductBox.vue';
 export default {
   name: 'HomeView',
   data() {
@@ -42,7 +29,9 @@ export default {
       latestProducts: [],
     };
   },
-  components: {},
+  components: {
+    ProductBox,
+  },
   mounted() {
     document.title = 'Home | Djackets';
     this.getLatestProducts();
@@ -63,10 +52,3 @@ export default {
   },
 };
 </script>
-<style>
-.image {
-  margin-top: -1.25rem;
-  margin-left: -1.25rem;
-  margin-right: -1.25rem;
-}
-</style>
--- ecommerce_django_vue/product/views.py
+++ ecommerce_django_vue/product/views.py
@@ -3,8 +3,8 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Category, Product
+from .serializers import ProductSerializer, CategorySerializer
 
 class LatestProductsList(APIView):
     def get(self, request, format=None):
@@ -22,4 +22,16 @@ class ProductDetail(APIView):
     def get(self, request, category_slug, product_slug, format=None):
         product = self.get_object(category_slug, product_slug)
         serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+class CategoryDetail(APIView):
+    def get_object(self, category_slug):
+        try:
+            return Category.objects.get(slug=category_slug)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category_slug, format=None):
+        cateogory = self.get_object(category_slug)
+        serializer = CategorySerializer(cateogory)
         return Response(serializer.data)
